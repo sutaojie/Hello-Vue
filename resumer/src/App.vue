cd20b2e03852bf648060d960ed66c81b6e94afd5
sutaojie <sutaojie@gmail.com>
2018-07-21 00:12:44 +0800

    cli-vue

---
+++ resumer/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <p>{{message}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data(){
+    return {
+      message:'Hello'
+    }
+  }
+}
+</script>
+
+<style>
+p{color:red;}
+</style>
