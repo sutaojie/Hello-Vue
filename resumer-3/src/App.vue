f0a5e34be36854b9dc7dd5e54a030213e82e896f
sutaojie <sutaojie@gmail.com>
2018-07-22 22:30:10 +0800

    简历初始态 v0.0.0

---
+++ resumer-3/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <p><span>wo</span>{{text}}</p>
+    {{text}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data () {
+    return { 
+      text : '!'
+    }
+  }
+}
+</script>
+
+<style lang='sass'>
+p
+  color:green
+  span
+    color:red
+
+</style>
