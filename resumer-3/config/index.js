4c4fcf2cdd9d93e24dddbbc98d42f1e32c8dc2ca
sutaojie <sutaojie@gmail.com>
2018-07-23 09:51:55 +0800

    修改 assetsPublicPath

--- resumer-3/config/index.js
+++ resumer-3/config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/sutaojie/resumer/dist',
+    assetsPublicPath: '/Hello-Vue/resumer-3/dist',
 
     /**
      * Source Maps
