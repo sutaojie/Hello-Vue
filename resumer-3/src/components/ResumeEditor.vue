3ba9ee305e0b267d247970423122490f11ae7cbd
sutaojie <sutaojie@gmail.com>
2018-07-24 07:16:42 +0800

    显示简介，工作经验，学历

--- resumer-3/src/components/ResumeEditor.vue
+++ resumer-3/src/components/ResumeEditor.vue
@@ -11,7 +11,7 @@
         </nav>
         <ol class="panels">
             <li v-for="item in resume.config" v-show="item.field === selected">
-                <div v-if='resume[item.field] instanceof Array'>
+                <div v-if='resume[item.field] instanceof Array' >
                    <div class="subitem" v-for="subitem in resume[item.field]">
                        <div class="resumeField" v-for='(value, key) in subitem'>
                             <label >{{key}}</label>
--- resumer-3/src/store/index.js
+++ resumer-3/src/store/index.js
@@ -7,23 +7,34 @@ export default new Vuex.Store({
             resume:{
                 config: [
                     {field: 'profile', icon: 'id'},
-                    {field: 'work history', icon: 'work'},
+                    {field: 'workHistory', icon: 'work'},
                     {field: 'education', icon: 'book'},
                     {field: 'projects', icon: 'heart'},
                     {field: 'awards', icon: 'cup'},
                     {field: 'contacts', icon: 'phone'},
                 ],
                 profile:{
-                    name:'1',
-                    city:'2',
-                    title:'3'
+                    name:'wang',
+                    city:'anhui',
+                    title:'resume',
+                    birthday:'1995-3-3'
                 },
-                'work history':[
-                    {company:'AL', content: '我的第二份工作是'},
-                    {company:'TX', content: '我的第一份工作是'}
-                    ],
+                workHistory:[ 
+                {
+                    company: '鸡飞狗跳公司', 
+                    content: `公司总部设在XXXX区，先后在北京、上海成立分公司。专注于移动XXX领域，主打产品XXXXX，它将资讯、报纸、杂志、图片、微信等众多内容，按照用户意愿聚合到一起，实现深度个性化 定制。
+                    我的主要工作如下:
+                    1. 完成既定产品需求。
+                    2. 修复 bug。`
+                },
+                {
+                    company: '狗急跳墙责任有限公司', 
+                    content: `公司总部设在XXXX区，先后在北京、上海成立分公司。专注于移动XXX领域，主打产品XXXXX，它将资讯、报纸、杂志、图片、微信等众多内容，按照用户意愿聚合到一起，实现深度个性化 定制。
+                    我的主要工作如下:1. 完成既定产品需求。2. 修复 bug`
+                }
+                ],
                 education: [
-                    {school: 'AL', content: '文字'}
+                    { school: '黄志诚警官大学', content: '本科' }, 
                 ],
                 projects: [
                     {name:'project A', content:'xxx'}
